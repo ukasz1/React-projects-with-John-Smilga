489cb30ea7bf1e1ea58ab4a2683115aab4ade7b1
ukasz1 <46789003+ukasz1@users.noreply.github.com>
2022-01-19 16:56:55 +0100

    final project

--- 10-grocery-bud/src/Alert.js
+++ 10-grocery-bud/src/Alert.js
@@ -1,7 +1,13 @@
 import React, { useEffect } from 'react'
 
-const Alert = () => {
-  return <h2>alert component</h2>
+const Alert = ({ type, msg, removeAlert, list }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [list]); //zamiast [] bo te 3 sek. muszą się liczyć od ostaniego kliknięcia a nie od 1-ego
+  return <p className={`alert alert-${type}`}>{msg}</p>
 }
 
 export default Alert
