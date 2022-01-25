f4017dd59c718a854298ae00bb59263007efb1a5
ukasz1 <46789003+ukasz1@users.noreply.github.com>
2022-01-25 13:05:14 +0100

    final project

--- 15-cocktails/src/pages/About.js
+++ 15-cocktails/src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget orci risus. Donec nec facilisis nisi. Quisque ultrices nibh velit, non tincidunt lacus accumsan non. Integer sed tincidunt erat, vitae pretium massa. Nullam quis purus in tellus rhoncus viverra non.
+      </p>
+    </section>
   )
 }
 
