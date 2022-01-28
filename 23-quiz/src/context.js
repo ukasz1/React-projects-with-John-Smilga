381b4ef237fbacbdd30b38cc4ab73bb74dfbb28c
ukasz1 <46789003+ukasz1@users.noreply.github.com>
2022-01-28 16:26:24 +0100

    questions and final modal added

--- 23-quiz/src/context.js
+++ 23-quiz/src/context.js
@@ -10,11 +10,86 @@ const table = {
 const API_ENDPOINT = 'https://opentdb.com/api.php?'
 
 const url = ''
+const tempUrl = 'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple '
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [waiting, setWaiting] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [error, setError] = useState(false);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchQuestions = async (url) => {
+    setLoading(true);
+    setWaiting(false);
+    const response = await axios(url).catch(err => console.log(err));
+    console.log(response);
+    if (response) {
+      const data = response.data.results;
+      if (data.length > 0) {
+        setQuestions(data);
+        setLoading(false);
+        setWaiting(false);
+        setError(false);
+      }
+      else {
+        setWaiting(true);
+        setError(true);
+      }
+    }
+    else {
+      setWaiting(true);
+    }
+  }
+
+  const nextQuestion = () => {
+    setIndex((oldIndex) => {
+      const index = oldIndex + 1;
+      if (index > questions.length - 1) {
+        openModal();
+        return 0;
+      } else {
+        return index;
+      }
+    })
+  }
+  const checkAnswer = value => {
+    if (value) {
+      setCorrect((oldState) => ++oldState)
+    }
+    nextQuestion();
+  }
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  }
+  const closeModal = () => {
+    setWaiting(true);
+    setCorrect(0);
+    setIsModalOpen(false);
+  }
+
+  useEffect(() => {
+    fetchQuestions(tempUrl);
+  }, []);
+
+  return <AppContext.Provider value={{
+    waiting,
+    loading,
+    questions,
+    index,
+    correct,
+    error,
+    isModalOpen,
+    nextQuestion,
+    checkAnswer,
+    closeModal
+  }}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
